8c54d2de80943f796e67fba16e5e02be9bdd9708
MadurangaDev <developer.shehan@gmail.com>
2025-09-03 17:44:10 +0530

    Maintenance - Module Alias Library Fix
    ## Fixes
    - Fixed module alias issue
    
    ## Improvements
    - Task card text ellipsis styles rewritten
    - Task card description text color updated as per the figma design

--- front-end/src/constants/colorTheme.ts
+++ front-end/src/constants/colorTheme.ts
@@ -1,3 +1,10 @@
 export const colorTheme = {
   primaryColor: "var(--primary-color)",
+  mainBlack: "var(---main-black)",
+  mainWhite: "var(--main-white)",
+  tabShadow: "var(--tab-shadow)",
+  secondaryBackground: "var(--secondary-background)",
+  gradientBlue: "var(--gradient-blue)",
+  gradientPurple: "var(--gradient-purple)",
+  mainGray: "var(--main-gray)",
 };
